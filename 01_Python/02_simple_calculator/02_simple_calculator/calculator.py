328fa6edfbcf6ca11d215fc379ede8d6c3da65e5
rewanebrahembadr-cmyk <rebrahemelbadry@gmail.com>
2026-07-06 00:04:49 +0300

    Create calculator.py

---
+++ 01_Python/02_simple_calculator/02_simple_calculator/calculator.py
@@ -0,0 +1,36 @@
+# Simple Calculator Project
+# This project helps beginners practice input, arithmetic operators, and conditions.
+
+print("Simple Calculator")
+print("Choose an operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter your choice (1/2/3/4): ")
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if choice == "1":
+    result = num1 + num2
+    print("Result:", result)
+
+elif choice == "2":
+    result = num1 - num2
+    print("Result:", result)
+
+elif choice == "3":
+    result = num1 * num2
+    print("Result:", result)
+
+elif choice == "4":
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Error: Cannot divide by zero.")
+
+else:
+    print("Invalid choice.")
